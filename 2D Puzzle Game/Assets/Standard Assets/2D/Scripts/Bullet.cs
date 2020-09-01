ea0fb002070126bb1d82135cdabfa423747366c6
grimnear3 <69186715+grimnear3@users.noreply.github.com>
2020-09-01 13:46:16 +0930

    Added new scripts
    Simple bullet system

---
+++ 2D Puzzle Game/Assets/Standard Assets/2D/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed =20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity=transform.right*speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(){
+        Destroy(gameObject);
+    }
+}
---
+++ 2D Puzzle Game/Assets/Standard Assets/2D/Scripts/Shoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bullet;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+            fire();
+            }
+    }
+    void fire()
+    {
+        Instantiate(bullet, firePoint.position, firePoint.rotation);
+    }
+}
