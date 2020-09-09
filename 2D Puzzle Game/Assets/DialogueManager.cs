3e5eca195434fcdc52d7d3784b5906ebdfc108f7
Ben Korcz <ben.korcz@gmail.com>
2020-09-10 01:34:14 +0930

    Experimented with UI elements
    Added the ability for text to appear and the trigger based on an object, the text now appears and disappears based on the trigger.

---
+++ 2D Puzzle Game/Assets/DialogueManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    public Animator animator;
+    public Text nameText;
+    public Text dialogueText;
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue){
+        // Debug.Log("Starting convo with " + dialogue.name);
+        animator.SetBool("IsOpen",true);
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences){
+            sentences.Enqueue(sentence);
+
+            
+        }
+        
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence(){
+        if(sentences.Count == 0){
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+        // Debug.Log(sentence);
+        // dialogueText.text = sentence;
+    }
+
+    IEnumerator TypeSentence (string sentence){
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray()){
+            dialogueText.text +=letter;
+            yield return new WaitForSeconds(0.08f);
+        }
+    }
+    public void EndDialogue(){
+        // Debug.Log("End of convo");
+        animator.SetBool("IsOpen",false);
+    }
+
+}
---
+++ 2D Puzzle Game/Assets/DialogueTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    public void TriggerDialogue(){
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    void OnTriggerEnter2D(Collider2D trigger){
+        GameObject player = GameObject.Find("Player");
+
+        if(player!= null){
+            TriggerDialogue();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D trigger){
+        GameObject player = GameObject.Find("Player");
+
+        if(player!= null){
+             FindObjectOfType<DialogueManager>().EndDialogue();
+        }
+    }
+}
