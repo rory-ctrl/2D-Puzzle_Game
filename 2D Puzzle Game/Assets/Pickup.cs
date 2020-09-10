c6188498945f2f0f505f42577c9a0a49e6eb5234
grimnear3 <69186715+grimnear3@users.noreply.github.com>
2020-09-10 14:02:44 +0930

    Added Comments

--- 2D Puzzle Game/Assets/Pickup.cs
+++ 2D Puzzle Game/Assets/Pickup.cs
@@ -6,8 +6,7 @@ public class Pickup : MonoBehaviour
 {
     
 
-    //public Transform WeaponPoint;
-    public float attackRange=0.5f;
+
     GameObject item;
     public Transform WeaponPoint;
     public Transform PickupPoint;
@@ -17,27 +16,34 @@ public class Pickup : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+
         if(Input.GetKeyDown("e")){
            if(item!=null){
+               //Places a copy of the currently held wepaon on the ground before deleteing it from the player
                foreach (Transform child in transform)
                     {
                     Instantiate(child.gameObject,PickupPoint.position,PickupPoint.rotation);
                     Destroy(child.gameObject);
                      }
+                //Places a copy of the item being picked up as a child of the player
                Instantiate(item,WeaponPoint.position,WeaponPoint.rotation).transform.parent=gameObject.transform;
                }
+               //Destroys the item the player picked up
            Destroy(item);
         }
     }
+
+    //If the object being collided with is tagged as a weapon it is stored in a game object
+    //This allows the object to be instatiated if needed
     void OnTriggerEnter2D(Collider2D target){
-        Debug.Log("test");
         if(target.tag=="Weapon"){
            item=target.gameObject;
         }
     }
+    //Removes the game object from the variable so the player cannot pickup items when they are not in range
     void OnTriggerExit2D(Collider2D target){
          if(target.tag=="Weapon"){
            item=null;
