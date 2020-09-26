f255db0ff891742ae84a5c9081620f447071c983
rory-ctrl <rory-paul-mackin47@hotmail.com>
2020-09-26 16:19:42 +0930

    Basic Pause Menu and Start Menu Completed
    Basic Pause Menu and Start Menu Completed

---
+++ 2D Puzzle Game/Assets/PauseMenuBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuBehaviour : MainMenuBehaviour
+{
+    public static bool isPaused;
+    public GameObject pauseMenu;
+
+    private void Start()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyUp("escape"))
+        {
+            isPaused = true;
+            Time.timeScale = (isPaused) ? 0 : 1;
+            pauseMenu.SetActive(isPaused);
+        }
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+
+    }
+}
