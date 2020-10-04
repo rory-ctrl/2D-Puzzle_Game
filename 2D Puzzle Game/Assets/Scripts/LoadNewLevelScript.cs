812f8f51f71669cadb1994e9beaa8aa910471b38
rory-ctrl <rory-paul-mackin47@hotmail.com>
2020-10-04 15:42:31 +1030

    Uploaded New Level Trigger Script
    Uploaded New Level Trigger Script

--- 2D Puzzle Game/Assets/Scripts/LoadNewLevelScript.cs
+++ 2D Puzzle Game/Assets/Scripts/LoadNewLevelScript.cs
@@ -10,7 +10,7 @@ public class LoadNewLevelScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.CompareTag("Player"))
         {
             SceneManager.LoadScene(LevelName);
         }
