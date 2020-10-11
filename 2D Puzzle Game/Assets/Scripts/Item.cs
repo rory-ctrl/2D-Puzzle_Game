e1b3b3aef638a6edc8fabd3f84feea88b69a83ef
Ben Korcz <ben.korcz@gmail.com>
2020-10-11 14:27:41 +1030

    work on inventory

--- 2D Puzzle Game/Assets/Scripts/Item.cs
+++ 2D Puzzle Game/Assets/Scripts/Item.cs
@@ -2,13 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item
+public class Item : MonoBehaviour
 {
+    [SerializeField] private UI_Inventory inventory;
+
+    // public void Awake(){
+    //     inventory = GameObject.Find("UI_TopBar");
+    // }
+
+    [SerializeField]
     public enum ItemType {
         Weapon,
+        Bullet,
         Currency
     }
 
     public ItemType itemType;
+
+    [SerializeField]
     public int amount;
+
+    void OnTriggerEnter2D(Collider2D trigger){
+        GameObject player = GameObject.Find("TimeAgentPlayer");
+
+        if(player!= null){
+            // Interact();
+            pickUp();
+        }
+    }
+
+    public void pickUp(){
+        if(this.itemType == ItemType.Currency){
+            inventory.addCurrency(amount);
+            Debug.Log(amount);
+            Destroy(this);
+        }
+    }
 }
--- 2D Puzzle Game/Assets/Scripts/UI_Inventory.cs
+++ 2D Puzzle Game/Assets/Scripts/UI_Inventory.cs
@@ -8,18 +8,36 @@ public class UI_Inventory : MonoBehaviour
     private Transform itemSlotContainer;
     private Transform itemSlotTemplate;
 
+    private int m_Currency;
+    private Item Weapon;
+    private Item Bullet;
+
     private void Awake(){
-        itemSlotContainer = transform.Find("ItemSlotContainer");
-        itemSlotTemplate = itemSlotContainer.Find("ItemSlotTemplate");
+        m_Currency = 0;
+        // itemSlotContainer = transform.Find("ItemSlotContainer");
+        // itemSlotTemplate = itemSlotContainer.Find("ItemSlotTemplate");
+    }
+
+    public void addCurrency(int amount){
+        m_Currency += amount;
     }
 
     public void setInventory(Inventory inventory){
         this.inventory = inventory;
+        RefreshInventoryItems();
     }
 
     private void RefreshInventoryItems(){
+        int x = 0;
+        int y = 0;
+        float itemSlotCellSize = 30f;
+
         foreach(Item item in inventory.GetItemList()){
-            
+            RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
+            itemSlotRectTransform.gameObject.SetActive(true);
+            itemSlotRectTransform.anchoredPosition = new Vector2(x*itemSlotCellSize, y*itemSlotCellSize);
+            x++;
+
         }
     }
 }
