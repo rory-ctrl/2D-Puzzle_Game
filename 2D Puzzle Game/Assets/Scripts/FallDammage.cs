553e52492a03b00a175cf47e7d4a69309a2cd86e
grimnear3 <69186715+grimnear3@users.noreply.github.com>
2020-10-26 09:13:52 +1030

    Fall Dammage works
    Fall Dammage works

---
+++ 2D Puzzle Game/Assets/Scripts/FallDammage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class FallDammage : MonoBehaviour
+{
+    public GameObject startposition;
+    public int damage=10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D target){
+        switch(target.tag){
+            case "Player":
+                target.gameObject.GetComponent<PlatformerCharacter2D>().TakeDamage(damage);
+                 target.gameObject.transform.position=startposition.transform.position;
+                break;
+            default:
+                break;
+        
+    }
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
