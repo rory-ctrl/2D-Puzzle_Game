b1beba22db6374d50e65296de2838f83fa5ab2f0
grimnear3 <69186715+grimnear3@users.noreply.github.com>
2020-10-26 11:42:55 +1030

    Added Disapearing Platforms

---
+++ 2D Puzzle Game/Assets/Scripts/Disapear.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disapear : MonoBehaviour
+{
+    private GameObject obj;
+    public Color clear=new Color(1f,1f,1f,.3f);
+    public Color solid=new Color(1f,1f,1f,1f);
+    public float interval = 0.5f;
+    public float timeRemaining;
+    bool isActive=true; 
+    // Start is called before the first frame update
+    void Start()
+    {
+    obj=gameObject;
+    timeRemaining=interval;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vanish();
+    }
+    void vanish(){
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        else{
+            //Hides Platform on interval of time
+            if(isActive==false){
+                obj.GetComponent<BoxCollider2D>().enabled=true;
+                obj.GetComponent<SpriteRenderer>().color=clear;
+            }
+            else{
+                obj.GetComponent<BoxCollider2D>().enabled=false;
+                obj.GetComponent<SpriteRenderer>().color=solid;
+            }
+            isActive=!isActive;
+            timeRemaining=interval;
+        }
+    }
+}
